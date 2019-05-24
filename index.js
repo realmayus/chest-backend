9fb85245aebf985d782e6517bf1d39c50e665c25
Tallerik <erik.klasse@gmail.com>
2019-05-24 16:29:29 +0200

    Little workaround with rest

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 var http = require("http");
+var error = {"requestOK": false, "error": "Not specified!"};
+
+
+function handleReq(req) {
+    var answer;
+    if(req.url.startsWith("/chest")) {
+        answer = {"requestOK": true, data: "Chest is the answer!"}
+    } else {
+        answer = error;
+    }
+    return answer;
+}
+
 
 http.createServer(function (request, response) {
     // Send the HTTP header
     // HTTP Status: 200 : OK
     // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    console.log("Request mit URL: " + request.url);
 
-    // Send the response body as "Hello World"
-    response.end('Hello World\n');
+    responsejson = handleReq(request);
+
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.write(JSON.stringify(responsejson));
+    response.end();
+
+
 }).listen(8081);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Gestartet auf port 8081');
