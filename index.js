b7af986c719a38b6c13e48e98d37a9fa9e29a5bd
Tallerik <erik.klasse@gmail.com>
2019-05-24 22:09:52 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require("http");
-var error = {"requestOK": false, "error": "Not specified!"};
 
+
+var error = {"requestOK": false, "error": "Not specified!"};
 
 function handleReq(req) {
     var answer;
@@ -14,19 +15,11 @@ function handleReq(req) {
 
 
 http.createServer(function (request, response) {
-    // Send the HTTP header
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
     console.log("Request mit URL: " + request.url);
-
     responsejson = handleReq(request);
-
     response.writeHead(200, {'Content-Type': 'application/json'});
     response.write(JSON.stringify(responsejson));
     response.end();
-
-
 }).listen(8081);
 
-// Console will print the message
 console.log('Gestartet auf port 8081');
